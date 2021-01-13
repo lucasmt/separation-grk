fb2da7179097f06d1d2a30388f4205a114bf4f5f
lucasmt <lmartinellitabajara@gmail.com>
2021-01-12 20:23:07 -0500

    Modified SeparationGrkStrategy to construct deterministic strategies

--- SeparationGrkStrategy.cpp
+++ SeparationGrkStrategy.cpp
@@ -5,24 +5,43 @@ namespace SGrk {
 SeparationGrkStrategy::SeparationGrkStrategy(
     std::shared_ptr<CUDD::Cudd> mgr,
     std::shared_ptr<VarMgr> vars,
-    CUDD::BDD winning_states,
-    CUDD::BDD reaching_moves,
-    CUDD::BDD cycle_covering_states)
+    MemorylessStrategy initial_strategy,
+    CycleCover cycle_cover,
+    PermissiveStrategy weak_fg_strategy)
 	  : mgr_(std::move(mgr)), vars_(std::move(vars))
-	  , winning_states_(std::move(winning_states))
-	  , reaching_moves_(std::move(reaching_moves))
-	  , cycle_covering_states_(std::move(cycle_covering_states)) {}
+	  , initial_strategy_(std::move(initial_strategy))
+	  , cycle_cover_(std::move(cycle_cover))
+	  , weak_fg_strategy_(std::move(weak_fg_strategy))
+	  , reaching_moves_(weak_fg_strategy_.WinningMoves() &
+	                    !cycle_cover_.CoveredRegion())
+	  , reachability_strategy_(
+	        MemorylessStrategy::Determinize(mgr_, vars_,
+	                                        weak_fg_strategy_.WinningMoves()))
+{}
 
 CUDD::BDD SeparationGrkStrategy::WinningStates() const {
-	return winning_states_;
+	return weak_fg_strategy_.WinningStates();
 }
-	
+
 CUDD::BDD SeparationGrkStrategy::CycleCoveringStates() const {
-	return cycle_covering_states_;
+	return cycle_cover_.CoveredRegion();
 }
 	
 CUDD::BDD SeparationGrkStrategy::ReachingMoves() const {
 	return reaching_moves_;
+}
+
+const MemorylessStrategy& SeparationGrkStrategy::GetInitialStrategy() const {
+	return initial_strategy_;
+}
+
+const MemorylessStrategy& SeparationGrkStrategy::GetReachabilityStrategy()
+	  const {
+	return reachability_strategy_;
+}
+
+const CycleStrategy& SeparationGrkStrategy::GetCycleStrategy() const {
+	return cycle_cover_.Strategy();
 }
 
 }
--- SeparationGrkStrategy.h
+++ SeparationGrkStrategy.h
@@ -5,6 +5,10 @@
 
 #include <cuddObj.hh>
 
+#include "CycleCover.h"
+#include "CycleStrategy.h"
+#include "MemorylessStrategy.h"
+#include "PermissiveStrategy.h"
 #include "VarMgr.h"
 
 namespace SGrk {
@@ -12,24 +16,32 @@ namespace SGrk {
 class SeparationGrkStrategy {
 	std::shared_ptr<CUDD::Cudd> mgr_;
 	std::shared_ptr<VarMgr> vars_;
-	CUDD::BDD winning_states_;
+	MemorylessStrategy initial_strategy_;
+	PermissiveStrategy weak_fg_strategy_;
+	CycleCover cycle_cover_;
 	CUDD::BDD reaching_moves_;
-	CUDD::BDD cycle_covering_states_;
+	MemorylessStrategy reachability_strategy_;
 
  public:
 	
 	SeparationGrkStrategy(
 	  std::shared_ptr<CUDD::Cudd> mgr,
 	  std::shared_ptr<VarMgr> vars,
-	  CUDD::BDD winning_states,
-	  CUDD::BDD reaching_moves,
-	  CUDD::BDD cycle_covering_states);
+	  MemorylessStrategy initial_strategy,
+	  CycleCover cycle_cover,
+	  PermissiveStrategy weak_fg_strategy);
 
 	CUDD::BDD WinningStates() const;
 	
-	CUDD::BDD ReachingMoves() const;
-	
 	CUDD::BDD CycleCoveringStates() const;
+
+	CUDD::BDD ReachingMoves() const;
+
+	const MemorylessStrategy& GetInitialStrategy() const;
+
+	const MemorylessStrategy& GetReachabilityStrategy() const;
+
+	const CycleStrategy& GetCycleStrategy() const;
 };
  
 }
